49a431acb29b269794757c882a68a49155910bf2
waleedsaifullah <waleedsaifullah786@gmail.com>
2023-10-29 16:50:08 +0500

    first commit

---
+++ components/CustomButton.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import React from 'react'
+
+const CustomButton = () => {
+    return (
+        <button
+            disabled={false}
+            type={"button"}
+            className={`custom-btn`}
+            onClick={() => {}}
+        >
+
+        </button>
+    )
+}
+
+export default CustomButton
